d6257a816696405843138db28ebe5b20f0cadc0e
Yanzengyong <709402944@qq.com>
2021-03-22 09:51:39 +0800

    Updates

---
+++ precache-manifest.0fd9b3ffcd9e2ae309655ffb8226abdb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "213c1ba54dc6965415e3",
+    "url": "build/2.213c1ba5.js"
+  },
+  {
+    "revision": "ee23d624683c0464f92b0d8d01851548",
+    "url": "build/2.213c1ba5.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7dce726b4bc5d5e8992",
+    "url": "build/bundle.a7dce726.js"
+  },
+  {
+    "revision": "b1ac105b42de9ab81325",
+    "url": "build/main.b1ac105b.js"
+  },
+  {
+    "revision": "d4d063fb4d53b208c09698e48b6cf2e6",
+    "url": "index.html"
+  },
+  {
+    "revision": "b1ac105b42de9ab81325",
+    "url": "static/css/main.fc8ff5ed.chunk.css"
+  }
+]);
